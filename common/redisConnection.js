45771fd72ae0863120018b96ed23aca90db78862
Brandon White <brandon.white@whitecho.com>
2014-03-11 09:09:49 +1000

    Updated Redis Error

--- common/redisConnection.js
+++ common/redisConnection.js
@@ -5,7 +5,7 @@
 if (process.env.REDIS_URL) {
 	//Example - redis://172.17.0.18:6379
 	
-	var redisString = process.env.REDISTOGO_URL;
+	var redisString = process.env.REDIS_URL;
 	var port = redisString.substr(redisString.lastIndexOf(":")+1);
 	var hostname = redisString.substring(redisString.lastIndexOf("/")+1, redisString.lastIndexOf(":"));
 
